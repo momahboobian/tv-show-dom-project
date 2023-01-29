7542d74778d33818aefed1c5e279e41ceca65131
Mo Mahboobian <momahboobian@gmail.com>
2023-01-29 17:07:06 +0000

    level 100

--- script.js
+++ script.js
@@ -4,9 +4,52 @@ function setup() {
   makePageForEpisodes(allEpisodes);
 }
 
+// function makePageForEpisodes(episodeList) {
+//   const rootElem = document.getElementById("root");
+//   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+// }
+
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
+const episodesContainer = document.getElementById("episodes-container");
+
+episodeList.forEach((episode) => {
+  const episodeCode = `S${("0" + episode.season).slice(-2)}E${("0" + episode.number).slice(-2)}`;
+  const episodeElement = `
+    <div class="episode">
+      <h2>${episode.name}</h2>
+      <div>${episodeCode}</div>
+      <img src="${episode.image.medium}" alt="${episode.name}">
+      <p>${episode.summary}</p>
+      <a href="${episode.url}">More information from TVMaze</a>
+    </div>
+  `;
+  episodesContainer.innerHTML += episodeElement;
+});
+};
 
 window.onload = setup;
+
+
+
+
+/*
+const episodesContainer = document.getElementById("episodes-container");
+
+fetch("https://api.tvmaze.com/shows/82/episodes")
+  .then((response) => response.json())
+  .then((episodes) => {
+    episodes.forEach((episode) => {
+      const episodeCode = `S${("0" + episode.season).slice(-2)}E${("0" + episode.number).slice(-2)}`;
+      const episodeElement = `
+        <div class="episode">
+          <h2>${episode.name}</h2>
+          <div>${episodeCode}</div>
+          <img src="${episode.image.medium}" alt="${episode.name}">
+          <p>${episode.summary}</p>
+          <a href="${episode.url}">More information from TVMaze</a>
+        </div>
+      `;
+      episodesContainer.innerHTML += episodeElement;
+    });
+  });
+  */
